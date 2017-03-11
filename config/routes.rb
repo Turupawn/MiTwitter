011459e3265dc902bc38b011690b9948e76900fa
Turupawn <ahmed.hn.43@gmail.com>
2017-03-11 15:40:27 -0600

    primer commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :tweets
+  root to: "tweets#index"
+
+	devise_for :users, controllers: {
+	  sessions: 'users/sessions',
+	  registrations: 'users/registrations'
+	}
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
